c96a95268c7e997e4317f74701ba1928bb6b6b0f
Essie64 <eskury@gmail.com>
2023-02-16 11:30:59 -0800

    a C program that prints

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+1 #include <stdio.h>
+2 /**
+3 * main - Entry point
+4 *
+5 * Return: Always 0 (Success)
+6 */
+7 int main(void)
+8 {
+9	puts("\"Programming is like building a multilingual puzzle");
+10	return (0);
+11 } 

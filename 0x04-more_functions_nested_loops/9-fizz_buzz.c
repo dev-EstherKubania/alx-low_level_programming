a2f849350385667916469e8a569771cfecb4458b
Essie64 <eskury@gmail.com>
2023-02-23 15:26:41 -0800

    replaces numbers with words

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,22 +10,24 @@
  */
 int main(void)
 {
-	int i;
-	char f[] = "Fizz";
-	char b[] = "Buzz";
-	char fb[] = "FizzBuzz";
-
-	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
-			printf("%s ", fb);
-		else if (i % 3 == 0)
-			printf("%s ", f);
-		else if (i % 5 == 0)
-			printf("%s ", b);
-		else
-			printf("%d ", i);
+	for (int i=1; i<=100; i++)
+	if (i%15 == 0)
+	{
+	printf("FizzBuzz ");
+	}	
+	else if ((i%3) == 0)
+	{
+	printf("Fizz ");
 	}
-	printf("\n");
-	return (0);
+	else if ((i%5) == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{	
+	printf("%d ", i);
+	}	
+	}
+	return 0;
 }

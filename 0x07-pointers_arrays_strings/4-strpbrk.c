7e9756fbdc9d62f21ec01f01a3c9a9a9c27e7278
Essie64 <eskury@gmail.com>
2023-03-06 09:10:12 -0800

    searches a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,24 @@
-#include "main.h"                                                                                                                    
-#include "stdio.h"                                                                                                                                     
-/**                                                                                                                                  
- * _strpbrk - Searches a string for any of a set of bytes.                                                                           
- * @s: The string to be searched.                                                                                                    
- * @accept: The set of bytes to be searched for.                                                                                     
- *                                                                                                                                   
- * Return: If a set is matched - a pointer to the matched byte.                                                                      
- *         If no set is matched - NULL.                                                                                              
- */                                                                                                                                  
+#include "main.h"
+#include "stdio.h"
+
+/**
+ * *_strpbrk - function declaration
+ * @s: string to search
+ * @accept: bytes to search
+ * Return: pointer or NULL
+ */
 char *_strpbrk(char *s, char *accept)                                                                                                   
 {
 	int i;
 
 	while (*s)
 	{
-		for (i = 0; accept[i]; i++)
-		{
-			if (*s == accept[i])
-				return (s);
-		}
-		s++;
+	for (i = 0; accept[i]; i++)
+	{
+	if (*s == accept[i])
+	return (s);
+	}
+	s++;
 	}
 	return (NULL);
 }

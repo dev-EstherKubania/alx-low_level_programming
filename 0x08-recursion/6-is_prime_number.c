997d285b71470ca631e2da0f3c8e3838b88942dc
Essie64 <eskury@gmail.com>
2023-03-08 12:30:07 -0800

    checks for prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -23,3 +23,28 @@ int is_prime_number(int n)
 		return (divisor);
 	}
 }
+
+/**
+ * divisible - function definition
+ * @my_num: the number to test
+ * @divisor: the divisor to use
+ * Description: checks the divisibility of a number
+ * Return: 0 for divisible, 1 for not
+ */
+
+int divisible(int my_num, int divisor)
+{
+	if (my_num % divisor == 0)
+	{
+		return (0);
+	}
+	else if (divisor == my_num / 2)
+	{
+		return (1);
+	}
+	else
+	{
+		return (divisible(my_num, divisor + 1));
+	}
+}
+

c27c00d674f66599307b58d00da38615bc2e01aa
Essie64 <eskury@gmail.com>
2023-03-16 03:52:07 -0700

    split string into words

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,10 +1,12 @@
-#ifndef MAIN_H		
+#ifndef MAIN_iH		
 #define MAIN_H
 		
 char *create_array(unsigned int size, char c);
 		
 int _putchar(char c);
-		
+
+char **strtow(char *str);
+
 char *_strdup(char *str);
 		
 char *str_concat(char *s1, char *s2);

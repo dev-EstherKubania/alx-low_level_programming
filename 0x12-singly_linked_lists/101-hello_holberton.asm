bedb0d579e26865800a47577a03a860e38eb262f
Essie64 <eskury@gmail.com>
2023-03-31 08:24:30 -0700

    Write a 64-bit program in assembly that prints Hello, Holberton, followed by a new line

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+section .data
+    message db 'Hello, Holberton', 0Ah, 0
+
+section .text
+    global main
+
+    extern printf
+
+main:
+    push message
+    mov  rdi, message
+    mov  rax, 0
+    call printf
+    add  rsp, 8
+
+    xor  eax, eax
+    ret
+

bffb9930eea95b592edad42ac5d5bc413ce5bb3a
dev-EstherKubania <eskury@gmail.com>
2023-04-10 09:56:52 -0700

    crackme3 password

--- 0x14-bit_manipulation/crackme3.sh
+++ 0x14-bit_manipulation/crackme3.sh
@@ -5,7 +5,7 @@ echo -e
 echo -e "Fetching start file ..."
 echo -e
 
-curl https://github.com/holbertonschool/0x13.c/blob/b2dce6991a499f451ffd5b9522ac5d94ecb7bb56/crackme3 -o start.xt
+curl https://github.com/holbertonschool/0x13.c/blob/master/crackme3 -o start.xt
 chmod u+x start.xt
 
 ./start.xt

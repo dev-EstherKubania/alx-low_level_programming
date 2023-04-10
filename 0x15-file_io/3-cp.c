e9daf09928aa4cafac46d5e0362dea6b5ec86d17
dev-EstherKubania <eskury@gmail.com>
2023-04-10 15:51:05 -0700

    a program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,7 +22,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, bytes_read, bytes_written;
+	int file_from, file_to, bytes_read, bytes_written;
 	char buf[BUF_SIZE];
 
 	if (argc != 3)
@@ -30,35 +30,35 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't read from file_from %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-	if (fd_to == -1)
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	if (file_to == -1)
 	{
-		fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
+		fprintf(stderr, "Error: Can't write to file_to %s\n", argv[2]);
 		exit(99);
 	}
-	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
+	while ((bytes_read = read(file_from, buf, BUF_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buf, bytes_read);
+		bytes_written = write(file_to, buf, bytes_read);
 		if (bytes_written != bytes_read)
 	{
-			fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
+			fprintf(stderr, "Error: Can't write to file_to %s\n", argv[2]);
 			exit(99);
 	}
 	}
 	if (bytes_read == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't read from file_from %s\n", argv[1]);
 		exit(98);
 	}
-	if (close(fd_from) == -1 || close(fd_to) == -1)
+	if (close(file_from) == -1 || close(file_to) == -1)
 	{
-		fprintf(stderr, "Error: Can't close file descriptor\n");
+		fprintf(stderr, "Error: Can't close fd\n");
 		exit(100);
 	}
 	return (0);

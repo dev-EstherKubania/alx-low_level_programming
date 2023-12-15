589ca7fdb64fa9777e2a9808060d5acc10931aca
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2023-12-15 23:01:09 +0300

    Create search_algos.h

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGORITHMS
+#define SEARCH_ALGORITHMS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+
+#endif

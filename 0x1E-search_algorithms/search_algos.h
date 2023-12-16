0687495ddd586df2a525fe9cadcf0a250f620bca
Esther Kubania <122841197+dev-EstherKubania@users.noreply.github.com>
2023-12-17 00:21:42 +0300

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
-void print_array(int *array, size_t start, size_t end)
 
 #endif
